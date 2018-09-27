a99a06558b2e985b257080baec76538291830150
Pierre Verkest <pverkest@anybox.fr>
2018-09-28 00:22:38 +0200

    build site in .build directory

--- ci/build.sh
+++ ci/build.sh
@@ -2,15 +2,15 @@
 
 set -e
 
-DEST_DIR=${DEST_DIR:-"${HOME}/build_doc"}
+DEST_DIR=${DEST_DIR:-".build/"}
+TRAVIS_BUILD_DIR=${TRAVIS_BUILD_DIR:-$(pwd)}
 
-if [ "$TRAVIS_BRANCH" = "master" ] && [ "$TRAVIS_PULL_REQUEST" = false ]; then
-    mkdir -p "${DEST_DIR}"
-    rsync -av --update "${TRAVIS_BUILD_DIR}/site/" "${DEST_DIR}"
-    cp -r "${TRAVIS_BUILD_DIR}/slides/img" "${DEST_DIR}/img"
-    cat ${TRAVIS_BUILD_DIR}/slides/*.md > "${DEST_DIR}/anyblok-book-prez.md"
-    touch "${DEST_DIR}/.nojekyll"
-    mkdir -p "${DEST_DIR}/js/"
-    # curl "https://raw.githubusercontent.com/gnab/remark/gh-pages/downloads/remark-latest.min.js" > "${DEST_DIR}/js/remark-latest.min.js"
-    # echo "domain to do"  > "${DEST_DIR}/CNAME"
-fi
+
+mkdir -p "${DEST_DIR}"
+rsync -av --update "${TRAVIS_BUILD_DIR}/site/" "${DEST_DIR}"
+cp -r "${TRAVIS_BUILD_DIR}/slides/img" "${DEST_DIR}/img"
+cat ${TRAVIS_BUILD_DIR}/slides/*.md > "${DEST_DIR}/anyblok-book-prez.md"
+touch "${DEST_DIR}/.nojekyll"
+mkdir -p "${DEST_DIR}/js/"
+# curl "https://raw.githubusercontent.com/gnab/remark/gh-pages/downloads/remark-latest.min.js" > "${DEST_DIR}/js/remark-latest.min.js"
+# echo "domain to do"  > "${DEST_DIR}/CNAME"
